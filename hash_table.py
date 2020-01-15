a7682926a13e18ce7b2c38e0c2429fcfe99a07fa
Linh Pham <thuytpham9@gmail.com>
2020-01-15 12:19:18 -0800

    Set up the problem

---
+++ hash_table.py
@@ -0,0 +1,9 @@
+# A Table is given. Table is similar to a python list. You can choose the length 
+# for the list
+
+# Everytime we add items (key, value) pairs, (key,value) pairs have to go in the 
+# table
+
+# A has function requires a key
+# pass they key value to the has funciton to find the idex -->
+# add to the has table using the idex
