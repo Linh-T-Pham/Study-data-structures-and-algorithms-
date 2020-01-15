56fc360e61f38aac5382d91cc6496a01d6535ef2
Linh Pham <thuytpham9@gmail.com>
2020-01-15 12:44:31 -0800

    Updated line19 to 55

--- hash_table.py
+++ hash_table.py
@@ -4,6 +4,53 @@
 # Everytime we add items (key, value) pairs, (key,value) pairs have to go in the 
 # table
 
-# A has function requires a key
-# pass they key value to the has funciton to find the idex -->
+# A hash function requires a key
+
+# Pass the key value to the has funciton to find the idex -->
 # add to the has table using the idex
+
+
+# Solution:
+
+# Data collision: Using LinkedlistNode to account for data collision 
+
+
+
+class Node:
+    """"Create Class Node """
+    def _init_(self, key, value):
+
+        self.key = key
+        self.value = value
+        self.next = None
+
+class HashTable:
+
+    def _init_(self):
+        self.capacity = 11
+        self.buckets =[None]*self.capacity
+
+    def hashKey(key):
+        return hash(key) % self.capacity
+
+""""
+>>> key = 'apple'
+>>> key
+'apple'
+>>> hash(key)
+1512600610774552938
+>>> cap = 11
+>>> h = hash(key)
+>>> h % cap
+1
+>>> h % cap
+1
+>>> key = 'banaba'
+>>> h % cap
+1
+>>> h = hash(key)
+>>> h % cap
+6
+>>> 
+"""
+
