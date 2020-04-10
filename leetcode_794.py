815530c0268e1a6393862526464cdd0912c8ddcd
linh <thuytpham9@gmail.com>
2020-04-09 22:35:59 -0700

    Add back_space.py

--- leetcode_794.py
+++ leetcode_794.py
@@ -14,21 +14,35 @@
     No more moves can be played if the game is over.
 
 
-    >>> ["O  ", "   ", "   "]
+    >>> ["o  ", "   ", "   "]
     false
 
-    >>> ["XOX", " X ", "   "]
+    >>> ["xox", " x ", "   "]
     false
 
-    >>> ["XXX", "   ", "OOO"]
+    >>> ["xxx", "   ", "ooo"]
     false
 
-    >>> ["XOX", "O O", "XOX"]
+    >>> ["xox", "o  o", "xox"]
     true
 """
 
 def validTicTacToe(board):
-
+    o_count = 0
+    x_count = 0
+    
+    for row in board:
+        for char in row: 
+            if char == "o": # count the number of O
+                o_count += 1
+            if char == "x": # count the number of X
+                x_count += 1
+        return o_count, x_count
+        
+def check_invalid_move(board, o_count, x_count):
+    if o_count > x_count or x_count - 1 > o_count:
+        return False # Check when the conditions false
+    if 
 
 
 
@@ -38,3 +52,52 @@ if __name__ == '__main__':
     import doctest
     if doctest.testmod().failed == 0:
         print("\n*** ALL TESTS PASSED. GO YOU!\n")
+
+
+
+# class Solution(object):
+#     def validTicTacToe(self, board):
+#         """
+#         :type board: List[str]
+#         :rtype: bool
+#         """
+#         Xcount, Ocount = 0, 0
+#         for i in range(len(board)):
+#             for j in range(len(board[0])):
+#                 if board[i][j] == 'x':
+#                     Xcount += 1
+#                 if board[i][j] == 'O':
+#                     Ocount += 1
+#         if Ocount > Xcount or Xcount-1>Ocount:
+#                 return False
+        
+#         if Ocount >= 3:
+#             flagX,flagO = False,False
+#             for i in range(len(board)):
+                # if board[i] == 'xxx' or [x[i] for x in board] == ['X','X','X']:
+#                     flagX = True
+#             if board[0][2] == 'X' and board[1][1] == 'X' and board[2][0] == 'X':
+#                 flagX = True
+#             if board[0][0] == 'X' and board[1][1] == 'X' and board[2][2] == 'X':
+#                 flagX = True
+#             if Xcount == Ocount and flagX:
+#                 return False
+#             for i in range(len(board)):
+#                 if board[i] == 'OOO' or [x[i] for x in board] == ['O','O','O']:
+#                     flagO = True
+#                     if flagX:
+#                         return False
+#             if board[0][2] == 'O' and board[1][1] == 'O' and board[2][0] == 'O':
+#                     flagO = True
+#                     if flagX:
+#                         return False
+#             if board[0][0] == 'O' and board[1][1] == 'O' and board[2][2] == 'O':
+#                     flagO = True
+#                     if flagX:
+#                         return False
+#             if flagO:
+#                 if Xcount - 1 == Ocount:
+#                     return False
+                    
+#         return True
+# 			```	
