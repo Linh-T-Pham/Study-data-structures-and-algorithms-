6f5c83a670b4a6582782603fa7dbe59003c0ecea
linh <thuytpham9@gmail.com>
2020-04-23 12:31:42 -0700

    Added and solved

---
+++ hay_stack.py
@@ -0,0 +1,40 @@
+
+"""Given list (haystack), return index (0-based) of needle in the list.
+
+    Return None if needle is not in haystack.
+
+    Do this with recursion. You MAY NOT USE A `for` OR `while` LOOP.
+
+  For example:
+
+    >>> recursive_index(5, [1, 3, 5, 2, 4])
+    2
+
+    >>> recursive_index("hey", ["hey", "there", "you"])
+    0
+
+    >>> recursive_index("you", ["hey", "there", "you"])
+    2
+
+    >>> recursive_index("zork", ["foo", "bar", "baz"]) is None
+    True
+
+"""
+
+# Create a function take 2 arguments (char, array, start=0)
+# array is not empty
+# base case: 
+# 1. found it --> return idx
+# 2. reached end of list, and didn't find it --> return None
+
+
+
+def recursive_idx(needle, haystack, start=0):
+    if start == len(haystack):
+        return None
+    
+    if needle == haystack[start]:
+        return start
+    
+    # start += 1
+    return recursive_idx(needle, haystack, start + 1)
