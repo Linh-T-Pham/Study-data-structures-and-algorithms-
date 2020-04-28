9bb6fcc44bc8c15b1d27c46038c1a0eb30f68c01
linh <thuytpham9@gmail.com>
2020-04-27 19:19:14 -0700

    Solved

--- mode.py
+++ mode.py
@@ -17,6 +17,30 @@ If there is a tie, return all::
 
 def mode(nums):
     """Find the most frequent num(s) in nums."""
+     """Find the most frequent num(s) in nums."""
+
+    # START SOLUTION
+
+    # Make dictionary of {num:frequency}
+
+    num_count = {}
+
+    for num in nums:
+        num_count[num] = num_count.get(num, 0) + 1
+
+    # Find the *count* of highest letter
+
+    highest_count = max(num_count.values())
+
+    # For every number with that count, add to set of mode
+
+    mode = set()
+
+    for num, count in num_count.items():
+        if count == highest_count:
+            mode.add(num)
+
+    return mode
 
 
 if __name__ == '__main__':
