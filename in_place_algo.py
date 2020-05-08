ce776e3eb1728b19effcff71bcdd9093bb603657
linh <thuytpham9@gmail.com>
2020-05-07 18:02:45 -0700

    set up per_pal problem

--- in_place_algo.py
+++ in_place_algo.py
@@ -1,1 +1,4 @@
-  tail_of_list = my_list[1:]
+  # tail_of_list = my_list[1:]
+
+  # create a new list:
+  my_list[start_index:end_index]
---
+++ per_pal.py
@@ -0,0 +1,41 @@
+"""
+    Write an efficient function that checks 
+    whether any permutation ↴ of an input string is a palindrome. 
+    
+    >>> has_palindrome_permutation('civic')
+    True 
+
+    >>> has_palindrome_permutation('ivicc')
+    True
+
+    >>> has_palindrome_permutation('civil')
+    False
+
+    >>> has_palindrome_permutation('livci')
+    False 
+
+"""
+
+def has_palindrome_permutation(the_string):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. HOORAY!\n")
