21734ea743066248ae1f8844d5f883735f73c16e
linh <thuytpham9@gmail.com>
2020-05-10 12:54:35 -0700

    Set up the problem

---
+++ right_rotation.py
@@ -0,0 +1,27 @@
+"""
+    returns the rotated list by n 
+
+    >>> right_rotate([1,2,3,4,5,6], 3)
+    [4,5,6,1,2,3]
+
+    >>> right_rotate([1,2,3,4,5,6], 2)
+    [5,6,1,2,3,4]
+
+    >>> right_rotate([3,0,1,4,2,3], 3)
+    [4,2,3,3,0,1]
+
+"""
+    
+
+def right_rotate(lists, num):
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. HOORAY!\n")
