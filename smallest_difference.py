ef82b46faadf82984872bf01577d9279980451f6
linh <thuytpham9@gmail.com>
2020-05-11 09:35:31 -0700

    Set up the problem

---
+++ smallest_difference.py
@@ -0,0 +1,38 @@
+"""
+    Give 2 non-empty arrays of integers, finds the pair of numbers
+    one from each array whose absolute difference is closet to zero and
+    returns an array containing those 2 numbers, with the number form the fist
+    array in the first position 
+
+    >>> find_small_differnce([-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17])
+    [28, 26]
+
+       
+"""
+
+def find_small_differnce(array1, array2):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. HOORAY!\n")
