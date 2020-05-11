2bac062733edfcd4fe437b3b5b353f86341672a7
linh <thuytpham9@gmail.com>
2020-05-10 17:37:54 -0700

    Solved

--- right_rotation.py
+++ right_rotation.py
@@ -2,18 +2,22 @@
     returns the rotated list by n 
 
     >>> right_rotate([1,2,3,4,5,6], 3)
-    [4,5,6,1,2,3]
-
-    >>> right_rotate([1,2,3,4,5,6], 2)
-    [5,6,1,2,3,4]
+    [4, 5, 6, 1, 2, 3]
 
     >>> right_rotate([3,0,1,4,2,3], 3)
-    [4,2,3,3,0,1]
+    [4, 2, 3, 3, 0, 1]
 
 """
     
 
-def right_rotate(lists, num):
+def right_rotate(input_list, num):
+    output_list = input_list[num:] + input_list[:num]
+    return output_list
+
+
+
+
+
 
 
 
