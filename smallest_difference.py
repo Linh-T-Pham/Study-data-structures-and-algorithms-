f8fe0223ac1c772cc7aedd1794809856c722e5c4
linh <thuytpham9@gmail.com>
2020-05-13 15:58:45 -0700

    Soved

--- smallest_difference.py
+++ smallest_difference.py
@@ -11,6 +11,45 @@
 """
 
 def find_small_differnce(array1, array2):
+    # sort the array in place 
+    array1.sort()
+    array2.sort()
+
+    index1 = 0
+    index2 = 0 
+
+    # Initialize the small_difference to an empty array
+    smallest_difference_pair = []
+
+    smallest = float('inf')
+    current = float('inf')
+
+    # Why I don't initize the largest to inf
+
+    while index1 < len(array1) and index2 < len(array2):
+        first_num = array1[index1]
+        second_num = array2[index2]
+
+        if first_num < second_num:
+            current = second_num - first_num
+            index1 += 1
+
+        elif second_num < first_num:
+            current = first_num - second_num
+            index2 += 1
+
+        else:
+            return [first_num, second_num]
+
+        if smallest > current:
+            smallest = current
+            smallest_difference_pair = [first_num, second_num]
+
+    return smallest_difference_pair
+
+
+
+
 
 
 
