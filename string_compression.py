4a8871a4c2f45e493ab2c491b3efe3ca69e90e96
linh <thuytpham9@gmail.com>
2020-05-19 16:40:56 -0700

    updated

--- string_compression.py
+++ string_compression.py
@@ -17,10 +17,11 @@
 """
 
 def string_compression(string):
-  
-    current_letter = string[0]
-    letter_count = 0
-    new_string_lst= []
+
+    current_letter = string[0] # assign the current current letter at index 0
+    letter_count = 0 # count how many time the letter occurs 
+    new_string_lst = []
+
     for idx, letter in enumerate(string):
         if letter == current_letter:
             letter_count += 1
@@ -32,14 +33,47 @@ def string_compression(string):
 
     new_string_lst.append(current_letter)
     new_string_lst.append(str(letter_count))
-
-    new_string = ''.join(new_string_lst)
-    original_string_len = len(string)
-    new_string_lst = len(new_string_lst)
-
-    if original_string_len < new_string_lst:
-        return string
+    new_string = "".join(new_string_lst)
     return new_string
+    
+    if len(new_string_lst) < len(string):
+        return(string)
+    return(new_string_lst)
+
+
+
+
+    
+
+
+
+
+
+
+
+
+    # current_letter = string[0]
+    # letter_count = 0
+    # new_string_lst= []
+    # for idx, letter in enumerate(string):
+    #     if letter == current_letter:
+    #         letter_count += 1
+    #     else:
+    #         new_string_lst.append(current_letter)
+    #         new_string_lst.append(str(letter_count))
+    #         current_letter = string[idx]
+    #         letter_count = 1
+
+    # new_string_lst.append(current_letter)
+    # new_string_lst.append(str(letter_count))
+
+    # new_string = ''.join(new_string_lst)
+    # # original_string_len = len(string)
+    # # new_string_lst = len(new_string_lst)
+
+    # if len(string) < len(new_string_lst):
+    #     return string
+    # return new_string
 
 
 
