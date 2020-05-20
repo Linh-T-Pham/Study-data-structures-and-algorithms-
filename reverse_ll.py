1b4df040d6f5fa259f69b2438ca71767f9bb3662
linh <thuytpham9@gmail.com>
2020-05-19 18:23:02 -0700

    updated

--- reverse_ll.py
+++ reverse_ll.py
@@ -27,18 +27,18 @@ def reverse(head):
   return previous
 
 
-def main():
-  head = Node(2)
-  head.next = Node(4)
-  head.next.next = Node(6)
-  head.next.next.next = Node(8)
-  head.next.next.next.next = Node(10)
+# def main():
+#   head = Node(2)
+#   head.next = Node(4)
+#   head.next.next = Node(6)
+#   head.next.next.next = Node(8)
+#   head.next.next.next.next = Node(10)
 
-  print("Nodes of original LinkedList are: ", end='')
-  head.print_list()
-  result = reverse(head)
-  print("Nodes of reversed LinkedList are: ", end='')
-  result.print_list()
+#   print("Nodes of original LinkedList are: ", end='')
+#   head.print_list()
+#   result = reverse(head)
+#   print("Nodes of reversed LinkedList are: ", end='')
+#   result.print_list()
 
 
-main()
+# main()
