4533e99c6ed38ca27c202d82b09c34ca43f3b633
linh <thuytpham9@gmail.com>
2020-06-01 17:50:32 -0700

    Solved

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,3 +1,18 @@
 # Write a function that takes in an array of integers and return
 #a sorted version of that array. Use the insertion sort algorithm
 # to sort the array. 
+
+
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        for i in range(1, len(nums)):
+            j = i  
+            while j > 0 and nums[j] < nums[j - 1]:
+                swap(j, j - 1, nums)
+                j = j - 1
+                
+        return nums
+    
+    def swap(i, j, nums):
+        nums[i], nums[j] = nums[j], nums[i]
