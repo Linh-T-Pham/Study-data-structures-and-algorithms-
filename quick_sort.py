069f13c2d6161a1461ad187446a68635d6f57699
linh <thuytpham9@gmail.com>
2020-06-05 15:54:28 -0700

    Set up the problem

---
+++ quick_sort.py
@@ -0,0 +1,4 @@
+"""Pick a pivot value which can be random or the first value in the array. 
+   All  smaller numbers to the pivot's left and all bigger numbers to the 
+   pivot's right then repeating the same two steps on both sides of the pivot
+   until the entire array is sorted """
