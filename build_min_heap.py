a49a738b648d3cc1fddc93139689e55a929cae2a
linh <thuytpham9@gmail.com>
2020-06-08 16:30:32 -0700

    Setup the problem

---
+++ build_min_heap.py
@@ -0,0 +1,8 @@
+# Implement a Minheap class that supports
+# Building a Min heap from an input array of integers
+# Inserting integers in the heap
+# Removing the heap's min/root value
+# Peek at the heap's minimum/root value
+# Sifting integers up and down the heap, which is to be used when
+# inserting and revmong values 
+# Note that heap should be represented in the form of an array
